83df50793d0212c45ae2813ff9f8b0a86399d1a0
Imran <imran.haider@wearecogworks.com>
2020-11-11 16:30:59 +0000

    feat: added composer, model and components for migration

---
+++ Source/BackOfficeContentBlocker.Core/Components/BackOfficeContentBlockerComponent.cs
@@ -0,0 +1,49 @@
+﻿using BackOfficeContentBlocker.Core.Migrations;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Migrations;
+using Umbraco.Core.Migrations.Upgrade;
+using Umbraco.Core.Scoping;
+using Umbraco.Core.Services;
+
+namespace BackOfficeContentBlocker.Core.Components
+{
+    public class BackOfficeContentBlockerComponent : IComponent
+    {
+        private readonly IScopeProvider _scopeProvider;
+        private readonly IMigrationBuilder _migrationBuilder;
+        private readonly IKeyValueService _keyValueService;
+        private readonly ILogger _logger;
+
+        public BackOfficeContentBlockerComponent(IScopeProvider scopeProvider, IMigrationBuilder migrationBuilder,
+            IKeyValueService keyValueService, ILogger logger)
+        {
+            _scopeProvider = scopeProvider;
+            _migrationBuilder = migrationBuilder;
+            _keyValueService = keyValueService;
+            _logger = logger;
+        }
+
+        public void Initialize()
+        {
+            // Create a migration plan for a specific project/feature
+            // We can then track that latest migration state/step for this project/feature
+            var migrationPlan = new MigrationPlan("BackOfficeContentBlocker");
+
+            // This is the steps we need to take
+            // Each step in the migration adds a unique value
+            migrationPlan.From(string.Empty)
+                .To<AddBackOfficeContentBlockerTable>("backofficecontentblocker-db");
+
+            // Go and upgrade our site (Will check if it needs to do the work or not)
+            // Based on the current/latest step
+            var upgrader = new Upgrader(migrationPlan);
+
+            upgrader.Execute(_scopeProvider, _migrationBuilder, _keyValueService, _logger);
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
---
+++ Source/BackOfficeContentBlocker.Core/Models/Schemas/BackOfficeContentBlockerSchema.cs
@@ -0,0 +1,21 @@
+﻿using NPoco;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace BackOfficeContentBlocker.Core.Models.Schemas
+{
+    [TableName("BackOfficeContentBlocker")]
+    [PrimaryKey("Id", AutoIncrement = true)]
+    [ExplicitColumns]
+    public class BackOfficeContentBlockerSchema
+    {
+        [PrimaryKeyColumn(AutoIncrement = true, IdentitySeed = 1)]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("UserEmail")] public string UserEmail { get; set; }
+
+        [Column("PageId")] public int PageId { get; set; }
+
+        [Column("TimeStamp")] public int TimeStamp { get; set; }
+    }
+}
