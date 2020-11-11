ec0ab91b26e6fa9e34cb868ec73a1e47a1b8aa98
Imran <imran.haider@wearecogworks.com>
2020-11-11 14:43:26 +0000

    feat: add api controller and services

---
+++ Source/BackOfficeContentBlocker.Core/Services/BackOfficeContentBlockerService.cs
@@ -0,0 +1,15 @@
+﻿namespace BackOfficeContentBlocker.Core.Services
+{
+    public interface IBackOfficeContentBlockerService
+    {
+        bool IsPageOccupied(string currentUserEmail, int pageId);
+    }
+
+    public class BackOfficeContentBlockerService : IBackOfficeContentBlockerService
+    {
+        public bool IsPageOccupied(string currentUserEmail, int pageId)
+        {
+            return true;
+        }
+    }
+}
---
+++ Source/BackOfficeContentBlocker.Web/Controllers/Api/BackOfficeContentBlockerApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Umbraco.Web.Editors;
+using Umbraco.Web.Mvc;
+
+namespace BackOfficeContentBlocker.Web.Controllers.Api
+{
+    [PluginController("ContentBlocker")]
+    public class BackOfficeContentBlockerApiController : UmbracoAuthorizedJsonController
+    {
+        [HttpGet]
+        public IHttpActionResult IndexEvents()
+        {
+            return Json("");
+        }
+    }
+}
