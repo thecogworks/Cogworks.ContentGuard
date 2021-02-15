f990c6ea9c4b4406f64bf329dbad002f9a99d420
Marcin Zajkowski <zajkowski.dev@gmail.com>
2021-02-15 13:14:45 +0100

    chore: restructured project solution

---
+++ src/Cogworks.ContentGuard.Core/Migrations/AddBackOfficeContentBlockerTable.cs
@@ -0,0 +1,29 @@
+﻿using BackOfficeContentBlocker.Core.Models.Schemas;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Migrations;
+
+namespace BackOfficeContentBlocker.Core.Migrations
+{
+    public class AddBackOfficeContentBlockerTable : MigrationBase
+    {
+        public AddBackOfficeContentBlockerTable(IMigrationContext context) : base(context)
+        {
+        }
+
+        public override void Migrate()
+        {
+            Logger.Debug<AddBackOfficeContentBlockerTable>("Running migration {MigrationStep}", "AddCommentsTable");
+
+            // Lots of methods available in the MigrationBase class - discover with this.
+            if (TableExists("BackOfficeContentBlocker") == false)
+            {
+                Create.Table<BackOfficeContentBlockerSchema>().Do();
+            }
+            else
+            {
+                Logger.Debug<AddBackOfficeContentBlockerTable>("The database table {DbTable} already exists, skipping",
+                    "BlogComments");
+            }
+        }
+    }
+}
---
+++ src/Cogworks.ContentGuard.Web/Controllers/Api/BackOfficeContentBlockerApiController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Http;
+using BackOfficeContentBlocker.Core.Services;
+using Umbraco.Web.Editors;
+using Umbraco.Web.Mvc;
+
+namespace BackOfficeContentBlocker.Web.Controllers.Api
+{
+    [PluginController("ContentBlocker")]
+    public class BackOfficeContentBlockerApiController : UmbracoAuthorizedJsonController
+    {
+        private readonly IBackOfficeContentBlockerService _contentBlockerService;
+
+        public BackOfficeContentBlockerApiController(IBackOfficeContentBlockerService contentBlockerService)
+        {
+            _contentBlockerService = contentBlockerService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult IsPageBlocked(string currentUserEmail, int pageId)
+        {
+            var isPageOccupied = _contentBlockerService.IsPageBlocked(currentUserEmail, pageId);
+
+            return Json(isPageOccupied);
+        }
+
+        [HttpGet]
+        public IHttpActionResult LockPage(string currentUserEmail, int pageId)
+        {
+            var pageLocked = _contentBlockerService.LockPage(currentUserEmail, pageId);
+
+            return Json(pageLocked);
+        }
+
+        [HttpGet]
+        public IHttpActionResult RemoveLock(string currentUserEmail)
+        {
+            var lockRemoved = _contentBlockerService.RemoveLock(currentUserEmail);
+
+            return Json(lockRemoved);
+        }
+    }
+}
