c264c8eb76d3ec6b790a10317d8e3553db032e9b
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-22 11:41:01 +0100

    ci: added build process

--- src/Cogworks.ContentGuard/Properties/AssemblyInfo.cs
+++ src/Cogworks.ContentGuard/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cogworks.ContentGuard.Web")]
+[assembly: AssemblyTitle("Cogworks.ContentGuard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cogworks.ContentGuard.Web")]
+[assembly: AssemblyCompany("Cogworks")]
+[assembly: AssemblyProduct("Cogworks.ContentGuard")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
